1ff904753c391116c9c0a82d9d5f0e91aaab0854
Mark Purser <mark.purser@ntlworld.com>
2015-10-03 19:33:56 +0100

    Record audio class

---
+++ python_src/record_wav.py
@@ -0,0 +1,58 @@
+"""PyAudio example: Record a few seconds of audio and save to a WAVE file."""
+
+import pyaudio
+import wave
+
+
+class AudioRecorder:
+
+    CHUNK = 1024
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 1
+    RATE = 44100
+    RECORD_SECONDS = 5
+    WAVE_OUTPUT_FILENAME = "output.wav"
+
+    def __init__(self):
+
+        self.p = pyaudio.PyAudio()
+
+        self.stream = self.p.open(format=self.FORMAT,
+                        channels=self.CHANNELS,
+                        rate=self.RATE,
+                        input=True,
+                        frames_per_buffer=self.CHUNK)
+
+        print("* recording")
+
+    def readChunks(self):
+
+        frames = []
+
+        for i in range(0, int(self.RATE / self.CHUNK * self.RECORD_SECONDS)):
+            data = self.stream.read(self.CHUNK)
+            frames.append(data)
+
+        print("* done recording")
+
+        return frames
+
+    def close(self, frames):
+
+        self.stream.stop_stream()
+        self.stream.close()
+        self.p.terminate()
+
+        wf = wave.open(self.WAVE_OUTPUT_FILENAME, 'wb')
+        wf.setnchannels(self.CHANNELS)
+        wf.setsampwidth(self.p.get_sample_size(self.FORMAT))
+        wf.setframerate(self.RATE)
+        wf.writeframes(b''.join(frames))
+        wf.close()
+
+
+recorder = AudioRecorder()
+
+frames = recorder.readChunks()
+
+recorder.close(frames)
