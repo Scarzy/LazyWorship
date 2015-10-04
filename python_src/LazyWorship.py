15ada38484672af87714781cae8be4ad7b43c38a
Thomas Scarsbrook <lazyworship@scarzybrook.co.uk>
2015-10-04 12:08:12 +0100

    Add the LPF on the lyric change

--- python_src/LazyWorship.py
+++ python_src/LazyWorship.py
@@ -6,6 +6,7 @@ import display
 import db_mysql
 import threading
 import record_wav
+import collections
 import dejavu_handler
 from dejavu.recognize import MicrophoneRecognizer
 
@@ -15,6 +16,8 @@ class LazyWorship():
         self.disp = display.display()
         self.djv = dejavu_handler.dejavu_handler()
         self.dejavu_mic = dejavu_mic
+        self.dq = collections.deque(maxlen=5)
+        self.last_song = ""
         if dejavu_mic:
             pass
         else:
@@ -45,7 +48,7 @@ class LazyWorship():
                 while len(self.buff) == 0:
                     self.buff = self.rwav.getWindowBuffer()
                 fname = self.djv.recognise(self.buff)
-            song, verse, part = self.fname_to_parts(fname)
+            song, verse, part = self.fname_to_parts(self.get_verse(fname))
             self.disp.update_text(self.db.get_lyrics(song, verse, part))
             
     def fname_to_parts(self, fname):
@@ -59,6 +62,17 @@ class LazyWorship():
                 verse = int(match.group(2))
                 part = int(match.group(3))
         return song, verse, part
+
+    def get_verse(self, value):
+        self.dq.append(value)
+        self.last_song
+        dq_summary = {s: sum(1 for i in self.dq if i == s) for s in self.dq}
+        modal_song = sorted(dq_summary.iteritems(), key=lambda x:x[1], reverse=True)[0]
+        if modal_song[1] < len(self.dq) / 2.0:
+            return self.last_song
+        else:
+            self.last_song = modal_song[0]
+            return modal_song[0]
 
     def import_prints(self, dirs, ext):
         for d in dirs:
